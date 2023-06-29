603d75bbe0076290ab74418c6a44c281b4732491
T0MLE <lelaurain.tom@gmail.com>
2023-06-29 14:52:18 +0200

    get all phones and filter

---
+++ frontend/src/components/filters/Filter.jsx
@@ -0,0 +1,215 @@
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+import "./filter.scss";
+
+function Filter({ filters, setFilters }) {
+  const [models, setModels] = useState([]);
+  const [brands, setBrands] = useState([]);
+
+  const ram = [2, 4, 6, 8, 10];
+  const stockage = [16, 32, 64, 128, 256, 512, 1024];
+
+  const [isOpen, setIsOpen] = useState({
+    filter1: false,
+    filter2: false,
+    filter3: false,
+    filter4: false,
+  });
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/evaluation")
+      .then((result) => setModels(result.data));
+
+    axios
+      .get("http://localhost:8000/evaluation/brands")
+      .then((result) => setBrands(result.data));
+  }, []);
+
+  const handleClick = (nb) => {
+    setIsOpen((prevState) => ({
+      ...prevState,
+      [nb]: !prevState[nb],
+    }));
+  };
+
+  const handleBrand = (e) => {
+    if (e.target.checked) {
+      setFilters((prevFilters) => {
+        const updatedFilters = [...prevFilters];
+        updatedFilters[0].push(e.target.value);
+        return updatedFilters;
+      });
+    } else {
+      setFilters((prevFilters) => {
+        const updatedFilters = [...prevFilters];
+        updatedFilters[0] = updatedFilters[0].filter(
+          (item) => item !== e.target.value
+        );
+        return updatedFilters;
+      });
+    }
+  };
+
+  const handleModel = (e) => {
+    if (e.target.checked) {
+      setFilters((prevFilters) => {
+        const updatedFilters = [...prevFilters];
+        updatedFilters[1].push(e.target.value);
+        return updatedFilters;
+      });
+    } else {
+      setFilters((prevFilters) => {
+        const updatedFilters = [...prevFilters];
+        updatedFilters[1] = updatedFilters[1].filter(
+          (item) => item !== e.target.value
+        );
+        return updatedFilters;
+      });
+    }
+  };
+
+  const handleRAM = (e) => {
+    if (e.target.checked) {
+      setFilters((prevFilters) => {
+        const updatedFilters = [...prevFilters];
+        updatedFilters[2].push(e.target.value);
+        return updatedFilters;
+      });
+    } else {
+      setFilters((prevFilters) => {
+        const updatedFilters = [...prevFilters];
+        updatedFilters[2] = updatedFilters[2].filter(
+          (item) => item !== e.target.value
+        );
+        return updatedFilters;
+      });
+    }
+  };
+  const handleStorage = (e) => {
+    if (e.target.checked) {
+      setFilters((prevFilters) => {
+        const updatedFilters = [...prevFilters];
+        updatedFilters[3].push(e.target.value);
+        return updatedFilters;
+      });
+    } else {
+      setFilters((prevFilters) => {
+        const updatedFilters = [...prevFilters];
+        updatedFilters[3] = updatedFilters[3].filter(
+          (item) => item !== e.target.value
+        );
+        return updatedFilters;
+      });
+    }
+  };
+  return (
+    <div className="filter-bar">
+      <p>Trier par filtres</p>
+      <div className="dropdown">
+        <span>Marque</span>
+        <span
+          className={!isOpen.filter1 ? "cross" : "closed"}
+          onClick={() => handleClick("filter1")}
+        ></span>
+      </div>
+      {isOpen.filter1 && (
+        <div className="checkboxes">
+          {brands.map((e) => {
+            return (
+              <div>
+                <input
+                  type="checkbox"
+                  name=""
+                  id=""
+                  value={e.brand}
+                  onChange={handleBrand}
+                />
+                <label htmlFor={e.brand}>{e.brand}</label>
+              </div>
+            );
+          })}
+        </div>
+      )}
+      <div className="dropdown">
+        <span>Models</span>
+        <span
+          className={!isOpen.filter2 ? "cross" : "closed"}
+          onClick={() => handleClick("filter2")}
+        ></span>
+      </div>
+      {isOpen.filter2 && (
+        <div className="checkboxes">
+          {models.map((e) => {
+            return (
+              <div>
+                <input
+                  type="checkbox"
+                  name=""
+                  id=""
+                  value={e.name}
+                  onChange={handleModel}
+                />
+                <label htmlFor={e.name}>{e.name}</label>
+              </div>
+            );
+          })}
+        </div>
+      )}
+      <div className="dropdown">
+        <span>RAM</span>
+        <span
+          className={!isOpen.filter3 ? "cross" : "closed"}
+          onClick={() => handleClick("filter3")}
+        ></span>
+      </div>
+      {isOpen.filter3 && (
+        <div className="checkboxes">
+          {ram.map((e) => {
+            return (
+              <div>
+                <input
+                  type="checkbox"
+                  name=""
+                  id=""
+                  value={e}
+                  onChange={handleRAM}
+                />
+                <label htmlFor={e}>{e} GO</label>
+              </div>
+            );
+          })}
+        </div>
+      )}
+      <div className="dropdown">
+        <span>Stockage</span>
+        <span
+          className={!isOpen.filter4 ? "cross" : "closed"}
+          onClick={() => handleClick("filter4")}
+        ></span>
+      </div>
+      {isOpen.filter4 && (
+        <div className="checkboxes">
+          {stockage.map((e) => {
+            return (
+              <div>
+                <input
+                  type="checkbox"
+                  name=""
+                  id=""
+                  value={e}
+                  onChange={handleStorage}
+                />
+                <label htmlFor={e}>{e} GO</label>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Filter;
---
+++ frontend/src/pages/Phones/Phones.jsx
@@ -0,0 +1,134 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Filter from "../../components/filters/Filter";
+import "./phones.scss";
+
+function Phones() {
+  const [phones, setPhones] = useState([]);
+  const [search, setSearch] = useState("");
+  const [filteredPhones, setFilteredPhones] = useState([]);
+  const [filters, setFilters] = useState([[], [], [], []]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/phones")
+      .then((result) => setPhones(result.data));
+  }, []);
+
+  useEffect(() => {
+    console.log(phones);
+  }, [phones]);
+
+  useEffect(() => {
+    if (
+      filters[0].length === 0 &&
+      filters[1].length === 0 &&
+      filters[2].length === 0 &&
+      filters[3].length === 0
+    ) {
+      setFilteredPhones(phones);
+      return;
+    }
+
+    setFilteredPhones(
+      phones.filter((phone) => {
+        const { brand, name, ram, storage } = phone;
+        return (
+          (filters[0].length === 0 || filters[0].includes(brand)) &&
+          (filters[1].length === 0 || filters[1].includes(name)) &&
+          (filters[2].length === 0 || filters[2].includes(ram.toString())) &&
+          (filters[3].length === 0 || filters[3].includes(storage.toString()))
+        );
+      })
+    );
+  }, [filters, phones]);
+
+  // useEffect(() => {
+  //   if (
+  //     filters[0].length == 0 &&
+  //     filters[1].length == 0 &&
+  //     filters[2].length == 0 &&
+  //     filters[3].length == 0
+  //   ) {
+  //     setFilteredPhones(phones);
+  //     return;
+  //   }
+  //   if (
+  //     filters[0].length ||
+  //     filters[1].length ||
+  //     filters[2].length ||
+  //     filters[3].length
+  //   ) {
+  //     setFilteredPhones(
+  //       phones.filter(
+  //         (e) =>
+  //           filters[0]?.some((f) => f === e.brand) &&
+  //           filters[1]?.some((g) => g = e.model) &&
+  //           filters[2]?.some((h) => f === e.ram) &&
+  //           filters[3]?.some((i) => g = e.storage)
+  //       )
+  //     );
+  //   }
+  //   if (phones.length) {
+  //     setFilteredPhones(phones);
+  //   }
+  // }, [filters, phones.length]);
+
+  return (
+    <div className="phone-page">
+      <Filter
+        setFilteredPhones={setFilteredPhones}
+        filteredPhones={filteredPhones}
+        setFilters={setFilters}
+        filters={filters}
+      />
+      <div className="phones-container">
+        <div>
+          {" "}
+          <h1>Nos téléphones</h1>
+          <input
+            className="searchbar"
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Chercher un téléphone"
+          />
+        </div>
+        <table>
+          <tr>
+            <th>Marque</th>
+            <th>Model</th>
+            <th>RAM</th>
+            <th>Stockage</th>
+            <th>État</th>
+            <th>Chargeur</th>
+            <th>Prix</th>
+            <th></th>
+          </tr>
+          {filteredPhones
+            .filter((item) =>
+              item?.name.toLowerCase().includes(search.toLowerCase())
+            )
+            .map((e) => {
+              return (
+                <tr>
+                  <td>{e.brand}</td>
+                  <td>{e.name}</td>
+                  <td>{e.ram}</td>
+                  <td>{e.storage}</td>
+                  <td>{e.condition}</td>
+                  <td>{e["charger_in"] ? "Oui" : "Non"}</td>
+                  <td>{e.price}</td>
+                  <td>
+                    <button>Vendre</button>
+                  </td>
+                </tr>
+              );
+            })}
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default Phones;
