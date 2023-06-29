148f1afa27898d54640b30dbe33f80140e624ca7
T0MLE <lelaurain.tom@gmail.com>
2023-06-29 15:41:15 +0200

    did back for phones

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -10,16 +10,12 @@ app.use(cors({ origin: "http://localhost:5173" }));
 app.use(helmet());
 app.use(cookieParser());
 
-const recipe_rooter = require("../modules/recipe");
-const session_rooter = require("../modules/session");
-const tag_rooter = require("../modules/tag");
 const user_rooter = require("../modules/user");
-const wine_rooter = require("../modules/wine");
+const phone_rooter = require("../modules/phones");
+const evaluation_rooter = require("../modules/evaluation");
 
-app.use("/recipes", recipe_rooter);
-app.use("/sessions", session_rooter);
-app.use("/tags", tag_rooter);
+app.use("/evaluation", evaluation_rooter);
 app.use("/users", user_rooter);
-app.use("/wines", wine_rooter);
+app.use("/phones", phone_rooter);
 
 module.exports = app;
