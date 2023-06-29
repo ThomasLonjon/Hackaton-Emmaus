8f648516e91e4b85e255326a4febe64a3a41284d
ornel77 <t.ornel@yahoo.fr>
2023-06-29 15:19:11 +0200

    retrieve value from form ok

--- frontend/src/components/evaluation/Evaluation.jsx
+++ frontend/src/components/evaluation/Evaluation.jsx
@@ -1,26 +1,192 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import "./Evaluation.scss";
 import axios from 'axios'
+
+import {os, storage, memory, network, condition} from './data'
+
+import iphone from '../../assets/iphone.png'
 
 
 function Evaluation() {
     const [optionBrand, setOptionBrand] = useState([])
+    const [optionModel, setOptionModel] = useState([])
+    const [smartphoneObj, setSmartphoneObj] = useState({
+        brand: "",
+        model: "",
+        os: "",
+        memory: "",
+        storage: "",
+        network: "",
+        condition: "",
+    })
+
     const url = import.meta.env.VITE_BACKEND_URL
+
+    const brandRef = useRef()
+    const modelRef = useRef()
+    const osRef = useRef()
+    const memoryRef = useRef()
+    const storageRef = useRef()
+    const networkRef = useRef()
+    const conditionRef = useRef()
+
+    
+
     useEffect(() => {
         axios.get(`${url}/evaluation/brands`)
-        .then(res => console.log(res))
-    })
+        .then(res => setOptionBrand(res.data))
+
+        axios.get(`${url}/evaluation`)
+        .then(res => setOptionModel(res.data))
+    }, [])
+
+
+    const handleClickToPageTwo = (e) => {
+        e.preventDefault()
+        const firstPage = document.querySelector('.first-page')
+        const secondPage = document.querySelector('.second-page')
+        const thirdPage = document.querySelector('.third-page')
+
+        firstPage.style.display = "none"
+        secondPage.style.display = "flex"
+        thirdPage.style.display = "none"
+    }
+
+    const handleClickToPageThree = (e) => {
+        e.preventDefault()
+        const firstPage = document.querySelector('.first-page')
+        const secondPage = document.querySelector('.second-page')
+        const thirdPage = document.querySelector('.third-page')
+
+        firstPage.style.display = "none"
+        secondPage.style.display = "none"
+        thirdPage.style.display = "flex"
+
+        setSmartphoneObj({
+            brand: brandRef.current.value,
+            model: modelRef.current.value,
+            os: osRef.current.value,
+            memory: memoryRef.current.value,
+            storage: storageRef.current.value,
+            network: networkRef.current.value,
+            condition: conditionRef.current.value,
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        
+
+        
+    }
+
   return (
-    <form action="">
+    <form onSubmit={handleSubmit}>
         <section className="container first-page">
             <p className="title">
             Remplissez les champs ci-dessous pour évaluer le smartphone :
             </p>
-            <label htmlFor="brand">
-                <select name="brand" id="brand">
+            <div className=" select-container">
+                <label htmlFor="brand"> Marque </label>
+                <select name="brand" id="brand" ref={brandRef}>
                     <option value=""></option>
+                    {
+                        optionBrand.map((brand, index) => <option key={index} value={brand.brand}> {brand.brand} </option>)
+                    }
                 </select>
-            </label>
+            </div>
+
+            <div className="model select-container">
+                <label htmlFor="model"> Modèle </label>
+                <select name="model" id="model" ref={modelRef}>
+                    <option value=""></option>
+                    {
+                        optionModel.map((model, index) => <option key={index} value={model.name}> {model.name} </option>)
+                    }
+                </select>
+            </div>
+
+            <div className="os select-container">
+                <label htmlFor="os"> Système d'exploitation </label>
+                <select name="os" id="os" ref={osRef}>
+                    <option value=""></option>
+                    {
+                        os.map((os, index) => <option key={os.name} value={os.name}> {os.name} </option>)
+                    }
+                </select>
+            </div>
+            <button className="next btn" onClick={handleClickToPageTwo} >Suivant</button>
+        </section>
+
+        <section className="container second-page">
+            <div className="flex-container">
+                <div className="left">
+                    <p className="title">
+                    Remplissez les champs ci-dessous pour évaluer le smartphone :
+                    </p>
+                    <div className="ram select-container">
+                        <label htmlFor="ram"> RAM </label>
+                        <select name="ram" id="ram" ref={memoryRef}>
+                            <option value=""></option>
+                            {
+                                memory.map((a) => <option key={a.capacity} value={a.capacity}> {a.capacity} Go </option>)
+                            }
+                        </select>
+                    </div>
+                    <div className="storage select-container">
+                        <label htmlFor="storage"> Stockage </label>
+                        <select name="storage" id="storage" ref={storageRef}>
+                            <option value=""></option>
+                            {
+                                storage.map((a) => <option key={a.capacity} value={a.capacity}> {a.capacity} Go </option>)
+                            }
+                        </select>
+                    </div>
+                    <div className="network select-container">
+                        <label htmlFor="network"> Réseau </label>
+                        <select name="network" id="network" ref={networkRef}>
+                            <option value=""></option>
+                            {
+                                network.map((a) => <option key={a.id} value={a.network}> {a.network} </option>)
+                            }
+                        </select>
+                    </div>
+                    <div className="condition select-container">
+                        <label htmlFor="condition"> Etat </label>
+                        <select name="condition" id="condition" ref={conditionRef}>
+                            <option value=""></option>
+                            {
+                                condition.map((a) => <option key={a.id} value={a.name}> {a.name} </option>)
+                            }
+                        </select>
+                    </div>
+                <button className="submit btn" onClick={handleClickToPageThree}>Valider</button>
+
+                </div>
+                <div className="right">
+                    <img src={iphone} alt="" />
+                </div>
+            </div>
+        </section>
+
+        <section className="container third-page">
+            <div className="flex-container">
+                <div className="left">
+                    <h2>Prix conseillé pour le <span className="smartphone">smartphone</span></h2>
+                    <p className="price">40 €</p>
+                    <div className="specs">
+                        <p>Modèle: {smartphoneObj.model}</p>
+                        <p>Ram: {smartphoneObj.memory}Go</p>
+                        <p>Stockage: {smartphoneObj.storage}Go</p>
+                        <p>Etat: {smartphoneObj.condition}</p>
+                    </div>
+                    <button className="submit btn">Valider</button>
+                </div>
+                <div className="right">
+                    <img src={iphone} alt="" />
+                </div>
+            </div>
         </section>
     </form>
   )
---
+++ frontend/src/components/evaluation/data.js
@@ -0,0 +1,108 @@
+export const os = [
+    {
+        id:1,
+        name: "Android 10"
+    },
+    {
+        id:2,
+        name: "Android 11"
+    },
+    {
+        id:3,
+        name: "iOS 14"
+    },
+    {
+        id:4,
+        name: "iOS 16"
+    },
+    {
+        id:4,
+        name: "iOS 99"
+    },
+]
+
+export const memory = [
+    {
+        id:1,
+        capacity: 2
+    },
+    {
+        id:2,
+        capacity:4
+    },
+    {
+        id:3,
+        capacity: 6
+    },
+    {
+        id:4,
+        capacity: 8
+    },
+    {
+        id:4,
+        capacity: 10
+    },
+]
+
+export const storage = [
+    {
+        id:1,
+        capacity:16
+    },
+    {
+        id:2,
+        capacity: 32
+    },
+    {
+        id:3,
+        capacity: 64
+    },
+    {
+        id:5,
+        capacity: 128
+    },
+    {
+        id:6,
+        capacity: 256
+    },
+    {
+        id:7,
+        capacity: 512
+    },
+    {
+        id:8,
+        capacity: 1024
+    },
+]
+
+export const network = [
+    {
+        id:1,
+        network: "4G"
+    },
+    {
+        id:2,
+        network:"5G"
+    },
+]
+
+export const condition =[
+    {
+        id:1,
+        name: "Moyen"
+    },
+    {
+        id:2,
+        name:"Bon"
+    },
+    {
+        id:3,
+        name:"Parfait"
+    },
+]
+
+// const ponderation = [
+//     memory: {
+//         8: 
+//     }
+// ]
